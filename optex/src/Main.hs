c4d40a0d75b864f37ee809301a16eecb005d9a5b
u0164744 <giovanni.cosmiano@thomsonreuters.com>
2019-03-19 10:41:27 -0400

    Sample use of optparse-applicative Julie Moronuki Applicative/Monadic Parsing

--- optex/src/Main.hs
+++ optex/src/Main.hs
@@ -2,17 +2,37 @@ module Main where
 
 import Options.Applicative as Opt
 import Data.Semigroup ((<>))
-import Data.Char
+import Data.Char (toUpper)
+
+-----------------------------------------------------------------------------------
+-- |
+-- Utilities and combinators for parsing command line options
+-- https://github.com/pcapriotti/optparse-applicative
+-- https://hackage.haskell.org/package/optparse-applicative
 
 -----------------------------------------------------------------------------------
 
-data Welcome = Welcome { name :: String }
+data Welcome = Welcome { name :: String
+                       , excited :: Bool }
 
 runWithOptions :: Welcome -> IO ()
 runWithOptions opts =
-  putStrLn $ "Enjoy the snow, " ++ name opts ++ "!"
+  putStrLn $ transform $ "Enjoy the snow, " ++ name opts ++ "!"
+  where transform = if excited opts then map toUpper else id
 
 -----------------------------------------------------------------------------------
+
+-- |
+-- e.g.
+-- $ stack exec -- optex Gio -e
+-- ENJOY THE SNOW, GIO!
+--
+-- $ stack exec -- optex Gio --excited
+-- ENJOY THE SNOW, GIO!
+--
+-- $ stack exec -- optex Gio -x
+-- Invalid option `-x'
+-- Usage: optex.EXE NAME [-e|--excited]
 
 main :: IO ()
 main = do
@@ -20,6 +40,9 @@ main = do
   execParser opts >>= runWithOptions
   where
     parser = Welcome <$> argument str (metavar "NAME")
+                     <*> switch (short 'e' <>
+                                long "excited" <>
+                                help "Run in excited mode.")
     opts = info parser mempty
 --     info :: Parser a -> InfoMod a -> ParserInfo a
 
